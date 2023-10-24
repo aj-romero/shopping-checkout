454cb9adcd454b91821992d847b804b2c0a169ea
Alber J Romero <alberjose.romero@gmail.com>
2023-10-23 20:10:14 -0600

    adding application properties and main calss for spring boot

--- src/main/java/com/ajromero/ShoppingApplication.java
+++ src/main/java/com/ajromero/ShoppingApplication.java
@@ -1,4 +1,13 @@
 package com.ajromero;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ShoppingApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApplication.class, args);
+    }
 }
